a5b72cbf0778c295df50017f95aab8c19580b83b
yondonrinchin <33393679+yondonrinchin@users.noreply.github.com>
2020-03-27 23:16:43 +0800

    the bugs are fixed

--- server.js
+++ server.js
@@ -2,72 +2,72 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
-const formatMessage = require('./utils/messages')
-const { 
-    userJoin, 
-    getCurrentUser, 
-    userLeave, 
-    getRoomUsers 
+const formatMessage = require('./utils/messages');
+const {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
 } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
-require('dotenv/config')
 
+//Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
-io.on('connection', socket => {
+const botName = 'Монгол чат бот '
 
+//Run when client connects
+io.on('connection', socket => {
     socket.on('joinRoom', ({ username, room }) => {
-        
         const user = userJoin(socket.id, username, room)
-        
+
         socket.join(user.room)
-        
+
+        //Welcome current user
+        socket.emit('message', formatMessage(botName, 'Монгол чат-д тавтай морил!'))
+
         //Broadcast when a user connects
-        const botName = 'Монгол чат бот '
-        socket.emit(
-            'message',
-            formatMessage(botName,'Өдрийн мэнд!, тавтай морил :)'))
-        //when a user connects
         socket.broadcast
         .to(user.room)
         .emit(
             'message',
-            formatMessage(botName,`${user.username}, өрөөнд нэвтэрлээ!`))
-        // send users and room info
+            formatMessage(botName, `${user.username}, өрөөнд орж ирлээ`)
+        )
+        //Send users and room info
         io.to(user.room).emit('roomUsers', {
             room: user.room,
             users: getRoomUsers(user.room)
         })
-    })
 
-    //listen for chatmessage
+    })
+    //Listen for chatMessage
     socket.on('chatMessage', msg => {
         const user = getCurrentUser(socket.id)
-        io.to(user.room).emit('message',formatMessage(user.username, msg))
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
     })
-    //runs when client disconnects
+
+    //Runs when client disconnects
     socket.on('disconnect', () => {
-        const botName = 'Монгол чат бот '
         const user = userLeave(socket.id)
-        
+
         if(user){
             io.to(user.room).emit(
                 'message',
-                formatMessage(botName, `${user.username}, өрөөнөөс гарлаа!`))
-            
-            // send users and room info
+                formatMessage(botName, `${user.username} өрөөнөөс гарлаа`)
+            )
+
+            //send users and room info
             io.to(user.room).emit('roomUsers', {
                 room: user.room,
                 users: getRoomUsers(user.room)
-        })
+            })
         }
     })
-
 })
+const PORT = process.env.PORT || 3000
 
-server.listen(process.env.PORT, () => {
-    console.log(`server running on ${process.env.PORT}`)
-})
+server.listen(PORT, () => console.log(`Server runs on port ${PORT}`))
